9b3f7138abe3b10fe3e9f5ba7e2cf4b0da896696
Talaat-Eldeqn <142076735+Talaat-Eldeqn@users.noreply.github.com>
2024-04-28 22:18:33 +0300

    Add files via upload

---
+++ File-project-main/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿using System.Diagnostics.Tracing;
+using WMPLib;
+
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btn_prev = new System.Windows.Forms.Button();
+            this.btn_open = new System.Windows.Forms.Button();
+            this.btn_next = new System.Windows.Forms.Button();
+            this.btn_pause = new System.Windows.Forms.Button();
+            this.btn_play = new System.Windows.Forms.Button();
+            this.btn_stop = new System.Windows.Forms.Button();
+            this.track_list = new System.Windows.Forms.ListBox();
+            this.player = new AxWMPLib.AxWindowsMediaPlayer();
+            this.Repeat_fun = new System.Windows.Forms.Button();
+
+            // Set form background color
+            this.BackColor = System.Drawing.Color.FromArgb(31, 31, 31);
+
+            // btn_prev
+            this.btn_prev.Location = new System.Drawing.Point(10, 10);
+            this.btn_prev.Size = new System.Drawing.Size(100, 40);
+            this.btn_prev.Text = "⏮️";
+            this.btn_prev.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_prev.BackColor = System.Drawing.Color.FromArgb(51, 51, 51);
+            this.btn_prev.ForeColor = System.Drawing.Color.White;
+            this.btn_prev.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_prev.FlatAppearance.BorderSize = 0;
+            this.btn_prev.UseVisualStyleBackColor = true;
+            this.btn_prev.Click += new System.EventHandler(this.btn_prev_Click);
+
+            // btn_open
+            this.btn_open.Location = new System.Drawing.Point(120, 10);
+            this.btn_open.Size = new System.Drawing.Size(100, 40);
+            this.btn_open.Text = "📁";
+            this.btn_open.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_open.BackColor = System.Drawing.Color.FromArgb(46, 204, 113);
+            this.btn_open.ForeColor = System.Drawing.Color.White;
+            this.btn_open.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_open.FlatAppearance.BorderSize = 0;
+            this.btn_open.UseVisualStyleBackColor = true;
+            this.btn_open.Click += new System.EventHandler(this.btn_open_Click);
+
+            // btn_next
+            this.btn_next.Location = new System.Drawing.Point(230, 10);
+            this.btn_next.Size = new System.Drawing.Size(100, 40);
+            this.btn_next.Text = "⏭️";
+            this.btn_next.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_next.BackColor = System.Drawing.Color.FromArgb(51, 51, 51);
+            this.btn_next.ForeColor = System.Drawing.Color.White;
+            this.btn_next.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_next.FlatAppearance.BorderSize = 0;
+            this.btn_next.UseVisualStyleBackColor = true;
+            this.btn_next.Click += new System.EventHandler(this.btn_next_Click);
+
+            // btn_pause
+            this.btn_pause.Location = new System.Drawing.Point(10, 60);
+            this.btn_pause.Size = new System.Drawing.Size(100, 40);
+            this.btn_pause.Text = "⏸️";
+            this.btn_pause.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_pause.BackColor = System.Drawing.Color.FromArgb(51, 51, 51);
+            this.btn_pause.ForeColor = System.Drawing.Color.White;
+            this.btn_pause.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_pause.FlatAppearance.BorderSize = 0;
+            this.btn_pause.UseVisualStyleBackColor = true;
+            this.btn_pause.Click += new System.EventHandler(this.btn_pause_Click);
+
+            // btn_play
+            this.btn_play.Location = new System.Drawing.Point(120, 60);
+            this.btn_play.Size = new System.Drawing.Size(100, 40);
+            this.btn_play.Text = "▶️";
+            this.btn_play.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_play.BackColor = System.Drawing.Color.FromArgb(46, 204, 113);
+            this.btn_play.ForeColor = System.Drawing.Color.White;
+            this.btn_play.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_play.FlatAppearance.BorderSize = 0;
+            this.btn_play.UseVisualStyleBackColor = true;
+            this.btn_play.Click += new System.EventHandler(this.btn_play_Click);
+
+            // btn_stop
+            this.btn_stop.Location = new System.Drawing.Point(230, 60);
+            this.btn_stop.Size = new System.Drawing.Size(100, 40);
+            this.btn_stop.Text = "⏹️";
+            this.btn_stop.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_stop.BackColor = System.Drawing.Color.FromArgb(51, 51, 51);
+            this.btn_stop.ForeColor = System.Drawing.Color.White;
+            this.btn_stop.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_stop.FlatAppearance.BorderSize = 0;
+            this.btn_stop.UseVisualStyleBackColor = true;
+            this.btn_stop.Click += new System.EventHandler(this.btn_stop_Click);
+
+            // track_list
+            this.track_list.FormattingEnabled = true;
+            this.track_list.HorizontalScrollbar = true;
+            this.track_list.Location = new System.Drawing.Point(10, 110);
+            this.track_list.Size = new System.Drawing.Size(320, 200);
+            this.track_list.BackColor = System.Drawing.Color.FromArgb(31, 31, 31);
+            this.track_list.ForeColor = System.Drawing.Color.White;
+            this.track_list.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.track_list.TabIndex = 6;
+            this.track_list.SelectedIndexChanged += new System.EventHandler(this.track_list_SelectedIndexChanged);
+
+            // player
+            this.player.Enabled = true;
+            this.player.Location = new System.Drawing.Point(340, 10);
+            this.player.Size = new System.Drawing.Size(620, 420);
+            this.player.BackColor = System.Drawing.Color.Black;
+            this.player.TabIndex = 0;
+            this.player.Visible = true;
+            
+
+
+
+            // Repeat_fun
+            this.Repeat_fun.Location = new System.Drawing.Point(340, 440);
+            this.Repeat_fun.Size = new System.Drawing.Size(100, 40);
+            this.Repeat_fun.Text = "🔁";
+            this.Repeat_fun.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Repeat_fun.BackColor = System.Drawing.Color.FromArgb(46, 204, 113);
+            this.Repeat_fun.ForeColor = System.Drawing.Color.White;
+            this.Repeat_fun.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Repeat_fun.FlatAppearance.BorderSize = 0;
+            this.Repeat_fun.UseVisualStyleBackColor = true;
+            this.Repeat_fun.Click += new System.EventHandler(this.button1_Click);
+
+            // Form1
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(970, 500);
+            this.Controls.Add(this.Repeat_fun);
+            this.Controls.Add(this.btn_stop);
+            this.Controls.Add(this.track_list);
+            this.Controls.Add(this.btn_open);
+            this.Controls.Add(this.btn_next);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.btn_pause);
+            this.Controls.Add(this.btn_prev);
+            this.Controls.Add(this.btn_play);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Modern Media Player";
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            this.ResumeLayout(false);
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer player;
+        private System.Windows.Forms.Button btn_prev;
+        private System.Windows.Forms.Button btn_open;
+        private System.Windows.Forms.Button btn_next;
+        private System.Windows.Forms.Button btn_pause;
+        private System.Windows.Forms.Button btn_play;
+        private System.Windows.Forms.ListBox track_list;
+        private System.Windows.Forms.Button btn_stop;
+        private System.Windows.Forms.Button Repeat_fun;
+    }
+}
---
+++ File-project-main/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WMPLib; // Windows Media Player Library
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Unsubscribe from any existing PlayStateChange event handlers and subscribe to a new one
+            player.PlayStateChange -= Player_PlayStateChange;
+            player.PlayStateChange += Player_PlayStateChange;
+
+            // Initialize shuffle to true
+            isShuffleEnabled = true;
+        }
+
+        private bool isShuffleEnabled = false; // Flag to indicate if shuffle is enabled
+        private bool isRepeatEnabled = false; // Flag to indicate if repeat is enabled
+        private string[] paths; // Array to store file paths
+        private string[] files; // Array to store file names
+
+        // Event handler for opening files
+        private void btn_open_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog
+            {
+                Multiselect = true, // Allow selecting multiple files
+                Filter = "Media Files|*.mpg;*.avi;*.wma;*.mov;*.wav;*.mp2;*.mp3;*.mp4;*.flv;*.m4a;*.mkv;|All Files|*.*"
+            };
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                files = ofd.SafeFileNames; // Get the names of the selected files
+                paths = ofd.FileNames; // Get the paths of the selected files
+
+                track_list.Items.Clear(); // Clear the list box
+
+                // Add the file names to the list box
+                for (int x = 0; x < files.Length; x++)
+                {
+                    track_list.Items.Add(files[x]);
+                }
+
+                // Shuffle the playlist if shuffle is enabled
+                if (isShuffleEnabled)
+                {
+                    ShufflePlaylist();
+                }
+            }
+        }
+
+        // Event handler for selecting a track from the list box
+        private void track_list_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (track_list.SelectedIndex >= 0 && track_list.SelectedIndex < paths.Length)
+            {
+                try
+                {
+                    // Play the selected track
+                    player.URL = paths[track_list.SelectedIndex];
+                    player.Ctlcontrols.play();
+
+                    // Unsubscribe from previous PlayStateChange event
+                    player.PlayStateChange -= Player_PlayStateChange;
+                    // Subscribe to the PlayStateChange event to handle track ending
+                    player.PlayStateChange += Player_PlayStateChange;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error playing the selected track: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        // Single Event handler for PlayStateChange event
+        private void Player_PlayStateChange(object sender, AxWMPLib._WMPOCXEvents_PlayStateChangeEvent e)
+        {
+            // Check if the current state is 'Media Ended'
+            if ((WMPPlayState)e.newState == WMPPlayState.wmppsMediaEnded)
+            {
+                // Decide the next track to play based on whether shuffle is enabled
+                int nextIndex = isShuffleEnabled ? GetNextShuffleIndex() : track_list.SelectedIndex + 1;
+
+                // Check if we've reached the end of the playlist
+                if (nextIndex >= track_list.Items.Count)
+                {
+                    if (isRepeatEnabled) // If repeat is enabled, either reshuffle or restart from the beginning
+                    {
+                        if (isShuffleEnabled)
+                            ShufflePlaylist(); // Reshuffle if shuffle is enabled
+                        nextIndex = 0; // Start from the beginning
+                    }
+                    else
+                    {
+                        return; // Stop playback or handle as needed
+                    }
+                }
+
+                // Set the next item as the current selection and play
+                track_list.SelectedIndex = nextIndex; // This will trigger 'track_list_SelectedIndexChanged' to play the next track
+            }
+        }
+
+        // Event handler for the Play button
+        private void btn_play_Click(object sender, EventArgs e)
+        {
+            player.Ctlcontrols.play();
+        }
+
+        // Event handler for the Pause button
+        private void btn_pause_Click(object sender, EventArgs e)
+        {
+            player.Ctlcontrols.pause();
+        }
+
+        // Event handler for the Next button
+        private void btn_next_Click(object sender, EventArgs e)
+        {
+            if (track_list.SelectedIndex < track_list.Items.Count - 1)
+            {
+                track_list.SelectedIndex++;
+            }
+        }
+
+        // Event handler for the Previous button
+        private void btn_prev_Click(object sender, EventArgs e)
+        {
+            if (track_list.SelectedIndex > 0)
+            {
+                track_list.SelectedIndex--;
+            }
+        }
+
+        // Event handler for the Stop button
+        private void btn_stop_Click(object sender, EventArgs e)
+        {
+            player.Ctlcontrols.stop();
+        }
+
+        // Event handler for the repeat button
+        private void button1_Click(object sender, EventArgs e)
+        {
+            isRepeatEnabled = !isRepeatEnabled; // Toggle the repeat state
+
+            // Update button text to reflect the new state
+            Repeat_fun.Text = isRepeatEnabled ? "Repeat: ON" : "Repeat: OFF";
+        }
+
+        // Helper method to get the next index for shuffle mode
+        private int GetNextShuffleIndex()
+        {
+            // Implement shuffle logic here to determine the next index
+            // For now, just proceed linearly as an example
+            return track_list.SelectedIndex + 1;
+        }
+
+        // Helper method to shuffle the playlist
+        private void ShufflePlaylist()
+        {
+            Random rnd = new Random();
+
+            // Fisher-Yates shuffle algorithm
+            int n = track_list.Items.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rnd.Next(n + 1);
+
+                // Swap items in the list box
+                var value = track_list.Items[k];
+                track_list.Items[k] = track_list.Items[n];
+                track_list.Items[n] = value;
+
+                // Swap paths as well to keep track_list and paths in sync
+                string tempPath = paths[k];
+                paths[k] = paths[n];
+                paths[n] = tempPath;
+            }
+
+            // Automatically play the first song in the shuffled playlist
+            track_list.SelectedIndex = 0;
+        }
+    }
+}
